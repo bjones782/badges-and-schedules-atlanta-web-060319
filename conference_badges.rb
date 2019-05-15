5749e30caa0d4c357d299d656654b88fe5ee4d81
Brooke Jones <bjones782@gmail.com>
2019-05-15 15:59:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,8 @@
 def badge_maker(name)
   return "Hello, my name is #{name}"
 end
+ badge_maker("Arel")
+end
 
 #Write a `batch_badge_creator` method that takes an array of names as an argument and returns an array of badge messages.
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
